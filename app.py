35897ef935980c60659cdebc246c1df342969072
ariella <ariellah9@gmail.com>
2025-08-21 19:44:17 -0700

    Updated submit and leaderboard logic

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def get_leaderboard():
 
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
